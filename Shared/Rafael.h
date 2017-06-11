6f909b21b348b698ae706c57ee565756193f81df
Oscar Fuentes <oscar@appwinit.com>
2017-06-10 22:11:59 -0400

    Added missing umbrella file.

---
+++ Shared/Rafael.h
@@ -0,0 +1,19 @@
+//
+//  Rafael.h
+//  Rafael
+//
+//  Created by Oscar Fuentes on 6/10/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rafael.
+FOUNDATION_EXPORT double RafaelVersionNumber;
+
+//! Project version string for Rafael.
+FOUNDATION_EXPORT const unsigned char RafaelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rafael/PublicHeader.h>
+
+
